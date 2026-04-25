aa15e83003df1a80af5c5bf0de51291c7f6e2aea
DaLexto <github.dalexto@ilexto.com>
2026-04-25 21:51:14 +0200

    chore: scaffold cli package

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+/**
+ * Neurex UI CLI entry point.
+ *
+ * This package will provide commands such as:
+ * - neurex-ui init
+ * - neurex-ui add
+ * - neurex-ui update
+ * - neurex-ui list
+ * - neurex-ui doctor
+ */
+
+console.log("Neurex UI CLI")
